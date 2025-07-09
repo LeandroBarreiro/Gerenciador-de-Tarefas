1fc47923d3681787ebfc6bd2997d3fa3570882c6
LeandroBarreiro <leandrobarreiro15@gmail.com>
2025-07-09 16:49:01 -0400

    modified:   src/App.jsx 	modified:   src/components/Tasks.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -61,6 +61,28 @@ function App() {
       isCompleted: false,
     },
   ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      //Preciso atualizar essa tarefa
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      //Não preciso atualizar essa tarefa
+
+      return task;
+    });
+    setTaks(newTasks);
+  }
+
+  function deleteTask(taskId) {
+    const deleteTask = task.map((task) => {
+      if (task.id === taskId) {
+      }
+    });
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
@@ -68,7 +90,7 @@ function App() {
           Gerenciador de Tarefas
         </h1>
         <AddTask />
-        <Tasks tasks={tasks} />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
       </div>
     </div>
   );
