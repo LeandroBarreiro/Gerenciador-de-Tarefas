acfc1c27e9a18736e9707b245eedf19db967944c
LeandroBarreiro <leandrobarreiro15@gmail.com>
2025-07-09 15:30:27 -0400

    modified:   eslint.config.js 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	new file:   postcss.config.js 	modified:   src/App.css 	modified:   src/App.jsx 	modified:   src/components/AddTask.jsx 	deleted:    src/components/Taks.jsx 	new file:   src/components/Tasks.jsx 	modified:   src/index.css 	modified:   src/main.jsx 	new file:   tailwind.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
+import { useState } from "react";
 import AddTask from "./components/AddTask";
-import Tasks from "./components/Taks";
+import Tasks from "./components/Tasks";
 
 // function App() {
 //   const [count, setCount] = useState(0)
@@ -39,11 +40,36 @@ import Tasks from "./components/Taks";
 
 // State (Estado)
 function App() {
+  const [tasks, setTaks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação.",
+      description:
+        "Estudar programação para se tornar um desenvolvedor full stack.",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar inglês.",
+      description: "Estudar inglês para se tornar fluente.",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar matemática.",
+      description: "Estudar matemática para ter um boa logica de computação.",
+      isCompleted: false,
+    },
+  ]);
   return (
-    <div>
-      <h1>Gerenciador de Tarefas</h1>
-      <AddTask />
-      <Tasks />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks tasks={tasks} />
+      </div>
     </div>
   );
 }
